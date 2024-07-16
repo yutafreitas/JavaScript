4670fc01a9e90fad3867feb55aa3aa05f0212d2f
yuta freitas <devyutalima@gmail.com>
2024-07-16 10:54:09 -0300

    Initial commit

---
+++ ex011/exe008.js
@@ -0,0 +1,6 @@
+var vel = 13
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if(vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log(`Dirija sempre usando o cinto de segunrança`)
